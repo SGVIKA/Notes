d74334255cef0befa184992f1665623088c4cda5
SGVIKA <sadser.vika@gmail.com>
2023-06-27 12:59:33 +0300

    .

--- Notebook/app/src/main/java/com/example/notebook/Adapter/NotesListAdapter.java
+++ Notebook/app/src/main/java/com/example/notebook/Adapter/NotesListAdapter.java
@@ -1,5 +1,7 @@
 package com.example.notebook.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,24 +11,69 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.notebook.Models.Notes;
+import com.example.notebook.NotesClickListener;
 import com.example.notebook.R;
 
+import java.util.List;
+
 public class NotesListAdapter extends RecyclerView.Adapter <NotesViewHolder>{
+
+    Context context;
+    List<Notes> list;
+
+    NotesClickListener listener;
+
+    public NotesListAdapter(Context context, List<Notes> list, NotesClickListener listener) {
+        this.context = context;
+        this.list = list;
+        this.listener = listener;
+    }
 
     @NonNull
     @Override
     public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new NotesViewHolder(LayoutInflater.from(context).inflate(R.layout.notes_list, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
 
+        holder.textView_title.setText(list.get(position).getTitle());
+        holder.textView_title.setSelected(true);
+
+        holder.textView_notes.setText(list.get(position).getNotes());
+
+        holder.textView_date.setText(list.get(position).getDate());
+        holder.textView_date.setSelected(true);
+
+        if (list.get(position).isPinned()) {
+            holder.imageView_pin.setImageResource(R.drawable.ic_pin);
+        }
+        else {
+            holder.imageView_pin.setImageResource(0);
+        }
+
+        holder.notes_conteiner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onClick(list.get(holder.getAdapterPosition()));
+            }
+        });
+
+        holder.notes_conteiner.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                listener.onLongClick(list.get(holder.getAdapterPosition()), holder.notes_conteiner);
+                return true;
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 }
 
--- Notebook/app/src/main/java/com/example/notebook/MainActivity.java
+++ Notebook/app/src/main/java/com/example/notebook/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.notebook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.notebook.Adapter.NotesListAdapter;
+import com.example.notebook.DataBase.RoomDB;
+import com.example.notebook.Models.Notes;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton fab_add;
+    NotesListAdapter notesListAdapter;
+    RoomDB database;
+    List<Notes> notes = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_home);
+        fab_add = findViewById(R.id.fab_add);
+
+        database = RoomDB.getInstance(this);
+        notes = database.mainDAO().getAll();
+
+        updateRecucler(notes);
+
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NotesTakerActivity.class);
+                startActivityForResult(intent, 101);
+            }
+        });
+}
+
+    private void updateRecucler(List<Notes> notes) {
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL));
+        notesListAdapter = new NotesListAdapter(MainActivity.this, notes, notesClickListener);
+        recyclerView.setAdapter(notesListAdapter);
+    }
+
+    private final NotesClickListener notesClickListener = new NotesClickListener(){
+
+        @Override
+        public void onClick(Notes notes) {
+
+        }
+
+        @Override
+        public void onLongClick(Notes notes, CardView cardView) {
+
+        }
     }
 }
