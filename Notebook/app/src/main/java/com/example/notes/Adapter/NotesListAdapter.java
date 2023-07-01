86224fa9ad5e62656c530a836cf6b005d0bc5681
SGVIKA <sadser.vika@gmail.com>
2023-07-01 13:29:50 +0300

    .

--- Notebook/app/src/main/java/com/example/notes/Adapter/NotesListAdapter.java
+++ Notebook/app/src/main/java/com/example/notes/Adapter/NotesListAdapter.java
@@ -1,4 +1,4 @@
-package com.example.notebook.Adapter;
+package com.example.notes.Adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -11,9 +11,9 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.notebook.Models.Notes;
-import com.example.notebook.NotesClickListener;
-import com.example.notebook.R;
+import com.example.notes.Models.Notes;
+import com.example.notes.NotesClickListener;
+import com.example.notes.R;
 
 import java.util.List;
 
@@ -21,7 +21,6 @@ public class NotesListAdapter extends RecyclerView.Adapter <NotesViewHolder>{
 
     Context context;
     List<Notes> list;
-
     NotesClickListener listener;
 
     public NotesListAdapter(Context context, List<Notes> list, NotesClickListener listener) {
@@ -44,31 +43,30 @@ public class NotesListAdapter extends RecyclerView.Adapter <NotesViewHolder>{
 
         holder.textView_notes.setText(list.get(position).getNotes());
 
-        holder.textView_date.setText(list.get(position).getDate());
-        holder.textView_date.setSelected(true);
+        holder.textView_data.setText(list.get(position).getDate());
+        holder.textView_data.setSelected(true);
 
-        if (list.get(position).isPinned()) {
-            holder.imageView_pin.setImageResource(R.drawable.ic_pin);
+        if (list.get(position).isPinned()){
+            holder.imageView_pin.setImageResource(R.drawable.pin_icon);
         }
         else {
             holder.imageView_pin.setImageResource(0);
         }
 
-        holder.notes_conteiner.setOnClickListener(new View.OnClickListener() {
+        holder.notes_container.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 listener.onClick(list.get(holder.getAdapterPosition()));
             }
         });
 
-        holder.notes_conteiner.setOnLongClickListener(new View.OnLongClickListener() {
+        holder.notes_container.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                listener.onLongClick(list.get(holder.getAdapterPosition()), holder.notes_conteiner);
+                listener.onLongClick(list.get(holder.getAdapterPosition()), holder.notes_container);
                 return true;
             }
         });
-
     }
 
     @Override
@@ -76,7 +74,7 @@ public class NotesListAdapter extends RecyclerView.Adapter <NotesViewHolder>{
         return list.size();
     }
 
-    public void filterList (List<Notes> filteredList){
+    public void filterList (List<Notes> filteredList) {
         list = filteredList;
         notifyDataSetChanged();
     }
@@ -84,18 +82,17 @@ public class NotesListAdapter extends RecyclerView.Adapter <NotesViewHolder>{
 
 class NotesViewHolder extends RecyclerView.ViewHolder {
 
-    CardView notes_conteiner;
-    TextView textView_title, textView_notes, textView_date;
-
+    CardView notes_container;
+    TextView textView_title, textView_notes, textView_data;
     ImageView imageView_pin;
 
     public NotesViewHolder(@NonNull View itemView) {
         super(itemView);
 
-        notes_conteiner = itemView.findViewById(R.id.notes_conteiner);
+        notes_container = itemView.findViewById(R.id.notes_container);
         textView_title = itemView.findViewById(R.id.textView_title);
         textView_notes = itemView.findViewById(R.id.textView_notes);
-        textView_date = itemView.findViewById(R.id.textView_date);
+        textView_data = itemView.findViewById(R.id.textView_data);
         imageView_pin = itemView.findViewById(R.id.imageView_pin);
     }
 }
